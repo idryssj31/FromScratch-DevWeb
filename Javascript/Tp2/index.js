4bc65381d945d2e266811800070a4337b62619ef
idryss judéaux <idryss3108@gmail.com>
2024-11-03 22:59:58 +0100

    [ADD] : Click event logic

---
+++ Javascript/Tp2/index.js
@@ -0,0 +1,34 @@
+// Selecteurs
+//document.querySelector("h4").style.background = "yellow";
+// const baliseHTML = document.querySelector("h4");
+// console.log(baliseHTML);
+
+// Click event
+const questionContainer = document.querySelector(".click-event");
+console.log(questionContainer);
+
+// pour border-radius le - n'est pas pris en compte donc Kamel Case
+questionContainer.computedStyleMap.borderRadius = "150px";
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+const response = document.querySelector("p");
+
+questionContainer.addEventListener("click", () => {
+  console.log("Click !");
+  questionContainer.style.borderRadius = "10px";
+  questionContainer.classList.toggle("question-clicked");
+});
+
+btn1.addEventListener("click", () => {
+  console.log("ok");
+  response.style.visibility = "visible";
+  response.style.background = "blue";
+});
+
+btn2.addEventListener("click", () => {
+  console.log("okk");
+  response.style.visibility = "visible";
+  response.style.background = "red";
+});
+
+//------------------------------------------/

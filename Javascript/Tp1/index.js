ab2bb4b4a2195a172129d35f9f12e328b79d6de4
idryss judéaux <idryss3108@gmail.com>
2024-11-03 17:39:16 +0100

    [Init] : initaite file and some example of variable

---
+++ Javascript/Tp1/index.js
@@ -0,0 +1,30 @@
+alert("salut js");
+
+let variable = "hello";
+
+console.log(variable);
+
+// var vieux JS
+var unTexte = "voici un texte";
+
+// Kamel Case
+let maSuperVariable = "Hello";
+
+// Aucune diff entre "" et ''
+let maVariable1 = "hello";
+
+let maVariable2 = "hello";
+
+// Let = la variable peut évoluer
+let unChiffre = 24;
+unChiffre = 22;
+
+let chaine = "je suis l'une des chaines de caractères";
+console.log(chaine);
+
+let nouvelleChaine =
+  "Chaine précédente : " + chaine + ". Voila c'était le contenu";
+
+// Concaténation avec guillements altg+7
+let autreConcatenation = `Chaine précédente : ${chaine}. Voila c\'était le contenu;`;
+console.log(autreConcatenation);

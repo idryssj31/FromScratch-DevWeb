70a7ab203d806c3ece7c50fddee31f75d316e604
idryss judéaux <idryss3108@gmail.com>
2025-01-08 15:21:21 +0100

    [ADD] : 3 mouse circle and visual effect

--- Javascript/Tp3/index.js
+++ Javascript/Tp3/index.js
@@ -5,3 +5,35 @@
 // S'assurer que les liens sont clickables
 
 // Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+
+const mouseMove = document.querySelectorAll(".mousemove");
+console.log(mouseMove);
+
+mouseMove.forEach((child) => {
+  window.addEventListener("mousemove", (e) => {
+    child.style.left = e.pageX + "px";
+    child.style.top = e.pageY + "px";
+  });
+});
+
+const mouseCursor = document.getElementById("cursor");
+const mouseMove1 = document.getElementById("mouse1");
+const mouseMove2 = document.getElementById("mouse2");
+
+const text = document.querySelectorAll("h1, h2, p, h3, li");
+console.log(text);
+
+text.forEach((child) => {
+  child.addEventListener("mouseover", () => {
+    mouseCursor.style.background = "yellow";
+    mouseMove1.style.mixBlendMode = "white";
+    mouseMove2.style.mixBlendMode = "difference";
+  });
+  
+  child.addEventListener("mouseout", () => {
+    mouseCursor.style.background = "blue";
+    mouseMove1.style.mixBlendMode = "normal";
+    mouseMove2.style.mixBlendMode = "normal";
+  });
+});
+

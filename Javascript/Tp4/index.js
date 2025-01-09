2bba8baa6ffc74477502d94ce1ce2c1f34fbad82
idryss judéaux <idryss3108@gmail.com>
2025-01-09 13:51:52 +0100

    [END] : Tp Scroll bar finish

--- Javascript/Tp4/index.js
+++ Javascript/Tp4/index.js
@@ -5,3 +5,20 @@
 // Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
 
 // Connaitre niveau de scroll (window.scrollY)
+
+const nav = document.querySelector("nav");
+console.log(nav);
+nav.style.top = "0";
+let scrollValue = window.scrollY;
+let finalValue = scrollValue;
+window.addEventListener("scroll", () => {
+  if (window.scrollY > scrollValue) {
+    nav.style.top = "-60" + "px";
+    finalValue = window.scrollY;
+    scrollValue = finalValue;
+    console.log("final value : " + finalValue);
+  } else if (window.scrollY < scrollValue) {
+    nav.style.top = "0";
+    scrollValue = window.scrollY;
+  }
+});

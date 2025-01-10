a96b9ba2bd13d2069dc128bd8343eaf50790a098
idryss judéaux <idryss3108@gmail.com>
2025-01-10 12:27:36 +0100

    [UPD] : Clean first part dev

--- Javascript/TpBubble/index.js
+++ Javascript/TpBubble/index.js
@@ -1,37 +1,46 @@
-// Bubble property
-const bubble = document.querySelector(".bubble");
-bubble.style.opacity = "1";
-let nbr = 0;
+// Initiate the bubble
+function initBubble() {
+  document.body.innerHTML += `
+  <div class = "bubble">
+  </div>
+`;
+  const bubbles = document.querySelectorAll(".bubble");
+  bubbles.forEach((child) => {
+    setBubble(child);
+    setBubbleLocation(child);
+  });
+}
 
-// Random generation of bubble size
+// Set bubble properties
+function setBubble(_child) {
+  _child.style.opacity = "1";
+  let nbr = 0;
+  nbr = getRandomNbr(80, 250) + "px";
+  _child.style.height = nbr;
+  _child.style.width = nbr;
+}
+
+// Init random position
+function setBubbleLocation(_child) {
+  let nbrVPosition = getRandomNbr(-30, 30);
+  _child.style.bottom = nbrVPosition + "px";
+  let nbrHPosition = getRandomNbr(0, 900);
+  _child.style.transform = `translate(${nbrHPosition}%`;
+  console.log("position");
+}
+
+// Function to generare random numbers
 function getRandomNbr(_min, _max) {
   return Math.random() * (_max - _min) + _min;
 }
 
-nbr = getRandomNbr(80, 250) + "px";
-bubble.style.height = nbr;
-bubble.style.width = nbr;
-
-// Random mouvement of the bubble
-bubble.style.transform = "translate(100%)";
-
 // Bubble life time management
-/*let interval = setInterval(() => {
-    setBubbleLocation();
-}, 1000);*/
+let interval = setInterval(() => {
+  initBubble();
+}, 4000);
 
+// Stop propagation
 window.addEventListener("click", () => {
   console.log("stop interval");
   clearInterval(interval);
 });
-
-// Init random position
-function setBubbleLocation() {
-  let nbrVPosition = getRandomNbr(-30, 30);
-  bubble.style.bottom = nbrVPosition + "px";
-  let nbrHPosition = getRandomNbr(0, 900);
-  bubble.style.transform = `translate(${nbrHPosition}%`;
-  console.log("position");
-}
-
-//setBubbleLocation();

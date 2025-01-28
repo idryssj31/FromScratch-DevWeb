c016fdf433bcfe8e7a8fd63fcaf77b75e46581fe
idryss judéaux <idryss3108@gmail.com>
2025-01-28 16:35:38 +0100

    [END] : Tp finish

--- Javascript/TpData/TpTodoList/script.js
+++ Javascript/TpData/TpTodoList/script.js
@@ -1,7 +1,20 @@
 const form = document.querySelector("form");
 let text;
 const list = document.getElementById("list");
-const todoChecker = () => {};
+
+const storeList = () => {
+  window.localStorage.todoList = list.innerHTML;
+};
+
+const getStoreList = () => {
+  if (window.localStorage.todoList) {
+    list.innerHTML = window.localStorage.todoList;
+  } else {
+    list.innerHTML = `<li>Cliquez sur un todo pour le supprimer</li>`;
+  }
+};
+
+window.addEventListener("load", getStoreList);
 
 form.addEventListener("input", (e) => {
   text = e.target.value;
@@ -10,18 +23,23 @@ form.addEventListener("input", (e) => {
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   addTodoList();
+  item.value = "";
+  storeList();
+});
+
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("checked")) {
+    e.target.remove();
+    localStorage.removeItem("value");
+  } else {
+    e.target.classList.add("checked");
+    console.log("add");
+  }
+  storeList();
 });
 
 const addTodoList = () => {
   const li = document.createElement("li");
   li.textContent = text;
   list.appendChild(li);
-  li.addEventListener("click", (e) => {
-    if ((e.target.classList == "checked")) {
-      li.remove(e);
-    } else {
-      e.target.classList.add("checked");
-      console.log("add");
-    }
-  });
 };

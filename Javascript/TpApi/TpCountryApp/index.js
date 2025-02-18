cec15baa92e473827eee20ac7c4916d983ecc1ec
idryss.judeaux <idryss3108@gmail.com>
2025-02-18 16:46:45 +0100

    [ADD] : fetch and display function

--- Javascript/TpApi/TpCountryApp/index.js
+++ Javascript/TpApi/TpCountryApp/index.js
@@ -1,3 +1,40 @@
+const input = document.getElementById("inputSearch");
+const result = document.querySelector(".countries-container");
+let countries = [];
+
+async function fetchCountry() {
+  await fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => (countries = data));
+  console.log(countries);
+  countryDisplay();
+}
+
+function countryDisplay() {
+  console.log("ee");
+  if (countries === null) {
+    result.innerHTML = "<h2>Aucun résultat</h2>";
+  } else {
+    countries.length = 12;
+    result.innerHTML = countries
+      .map(
+        (country) => `
+    <li class="card">
+        <img src="${country.flags.png}" alt="Flag of ${country.name.common}">
+        <h2>${country.name.common}</h2>
+        <p>${country.capital}</p>
+        <p>Population : ${country.population.toLocaleString()}</p>
+  `
+      )
+      .join("");
+  }
+}
+
+input.addEventListener("input", (e) => {
+  //countryDisplay();
+});
+fetchCountry();
+
 // 1 - Tester le lien de l'API dans le navigateur (https://restcountries.com/v3.1/all)
 
 // 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
@@ -7,7 +44,7 @@
 // 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
 
 // 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
-coutry.name.includes(inputSearch.value);
+//coutry.name.includes(inputSearch.value);
 
 // 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
 
